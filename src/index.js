5bc63dc2771264b660b584f3590e9fec0ce9b4a0
jCallon <jascallon99@gmail.com>
2019-09-25 17:30:03 -0700

    Moar stuff. Still working on functionality.

--- src/index.js
+++ src/index.js
@@ -51,27 +51,30 @@ class Footer extends React.Component{
 	}
 	handleClick(e){
 		console.log("Footer item clicked");
+    ReactDOM.render(<Page children={github} />, root);
 	}
 	render(){
 		return (	
 			<footer>
 				<ul>
 				  <ThemeChanger />
-					<li onClick="handleClick">
+					<li onClick={this.handleClick}>
 						<img src="icon/facebook.svg" alt="facebook" height="25pt" />
 					</li>
-					<li onClick="handleClick">
-						<img src="icon/discord.svg" alt="Discord" height="25pt" />
-					</li>
-					<li onClick="handleClick">
+					<li onClick={this.handleClick}>
 						<img src="icon/twitter.svg" alt="Twitter" height="25pt" />
+					</li>
+					<li onClick={this.handleClick}>
+						<img src="icon/github.svg" alt="GitHub" height="25pt" />
+					</li>
+					<li onClick={this.handleClick}>
+						<img src="icon/discord.svg" alt="Discord" height="25pt" />
 					</li>
 				</ul>
 			</footer>
 		);
 	}
 }
-
 
 class ThemeChanger extends React.Component{
 	constructor(props){
@@ -121,14 +124,13 @@ class ThemeChanger extends React.Component{
 	}
 }
 
-
 function Popup(props){
 	function handleClick(e){
 		ReactDOM.render(<Page />, root);
 	}
 	return (
-		<div class="popup">
-			<div class="header">
+		<div id="popup">
+			<div id="header">
 				{props.title}
 				<a onClick={handleClick}>&#10006;</a>
 			</div>
@@ -141,27 +143,16 @@ function SocialMedia(props){
 	return (
 		<div class="SocialMedia">
 			<img id="profileImg" src={props.profileImg} alt="Unable to load profile picture." />
-			<a href={props.url} target="_blank">{props.username}</a>
+      <p>
+        {props.role}
+        &nbsp;|&nbsp;
+			  <a href={props.url} target="_blank">{props.username}</a>
+      </p>
 		</div>
 	);
 }
 
-const twitter = 
-<SocialMedia
-	title="Twitter"
-	profileImg="https://pbs.twimg.com/profile_images/967808988879482880/tCuE8jn9_400x400.jpg"
-	url="https://twitter.com/michelleobama"
-	username="@michellobama"
-/>
-
-/*
-const popup = <Popup title="Twitter" children={twitter} />
-const page = <Page children={popup} />
-*/
-
-const page = <Page />
-
-ReactDOM.render(page, root);
+ReactDOM.render(<Page />, root);
 
 //===============================================================================================
 const gettingStarted = 
@@ -169,14 +160,15 @@ const gettingStarted =
   <h1>Welcome!</h1>
   <h2>Rulesets</h2>
   <p>Rulesets are a per-tournament, self-imposed set of rules to promote competition and fairness. Usually Smash tournaments rulesets restrict the following.</p>
-    <p>Stages you can pick<br />
-    Many stages have walkoffs which lead to degenerate play, and some platform and shapes can mess with moves and recoveries. For example, many characters with telelporting recoveries will have their recovery send them in the wrong direction because of a slant under the stage, when they otherwise would have made it back.</p>
 
-    <p>The order you can pick stages<br />
-    Even if we restrict the stages we can play, there's bound to be stages that favor some characters over others. In the spirit of fair competition, stage selection should not determine who wins. Many rulesets will not let the winner pick the next stage to prevent snowballing.</p>
+  <p>Stages you can pick<br />
+  Many stages have walkoffs which lead to degenerate play, and some platform and shapes can mess with moves and recoveries. For example, many characters with telelporting recoveries will have their recovery send them in the wrong direction because of a slant under the stage, when they otherwise would have made it back.</p>
 
-    <p>Stock count (usually three stocks)<br />
-    Between the many stocks of Melee due to its fast pace, and the few stocks of Smash Bros. for Wii U due to its slow pace, competetors have decided to settle in the middle for Smash Ultimate. Using stocks instead of time makes the playing field more level between characters who kill fast and characters who play slow, increasing the variety of characters viable for competition.</p>
+  <p>The order you can pick stages<br />
+  Even if we restrict the stages we can play, there's bound to be stages that favor some characters over others. In the spirit of fair competition, stage selection should not determine who wins. Many rulesets will not let the winner pick the next stage to prevent snowballing.</p>
+
+  <p>Stock count (usually three stocks)<br />
+  Between the many stocks of Melee due to its fast pace, and the few stocks of Smash Bros. for Wii U due to its slow pace, competetors have decided to settle in the middle for Smash Ultimate. Using stocks instead of time makes the playing field more level between characters who kill fast and characters who play slow, increasing the variety of characters viable for competition.</p>
 
   <p>Items<br />
   Items easily lead to snowballing.</p>
@@ -187,3 +179,106 @@ const gettingStarted =
   <h2>Enterring a tournament!</h2>
   <p>If you've never played competitive smash before and want to try it out, try going to a 'local'! A local is the smallest type of the tournament that registers on leaderboards. Because of their small size, at locals it's easier to make friends, they're usually less expensive, and they're shorter. Go to 'Calandars' to find a local right for you!</p>
 </div>
+
+const calandar = 
+<div>
+  <h1>Welcome!</h1>
+  <h2>Rulesets</h2>
+    <p>Stages you can pick<br />
+    Many stages have walkoffs which lead to degenerate play, and some platform and shapes can mess with moves and recoveries. For example, many characters with telelporting recoveries will have their recovery send them in the wrong direction because of a slant under the stage, when they otherwise would have made it back.</p>
+</div>
+
+const twitter = 
+<Popup title="Twitter" children={
+<SocialMedia
+	profileImg="https://pbs.twimg.com/profile_images/1120052132776796160/nbK9A9G7_400x400.jpg"
+  role="Lead Developer & Content Editor"
+	url="https://twitter.com/CeruleanAoi?s=09"
+	username="@CeruleanAoi"
+/>}/>
+
+const facebook = 
+<Popup title="facebook" children={
+<SocialMedia
+	profileImg=""
+  role=""
+	url=""
+	username=""
+/>}/>
+
+const discord = 
+<Popup title="Discord" children={
+<div>
+<SocialMedia
+	profileImg="https://cdn.discordapp.com/avatars/194312605941104640/817e8a5e5a0139076775833c44e16a9b.png"
+  role="Project Manager"
+	url="https://discordapp.com/"
+	username="UNITY | Everest#1614" />
+
+<SocialMedia
+	profileImg="https://cdn.discordapp.com/avatars/77311178010796032/f72bf2cee59e6b8fc22703fa01708d7b.png"
+  role="Developer Lead"
+	url="https://discordapp.com/"
+	username="Cyril#9605" />
+
+<SocialMedia
+	profileImg="https://cdn.discordapp.com/avatars/485620789643575307/651d326acb6c47e651548a4d53826a01.png"
+  role="Lead Developer"
+	url="https://discordapp.com/"
+	username="honeydew#8300" />
+
+<SocialMedia
+	profileImg="https://cdn.discordapp.com/avatars/210957217485029376/c76b6f904e8db259822e7696efa10213.png"
+  role="Human Database"
+	url="https://discordapp.com/"
+	username="giantrobotbee#0001" />
+
+<SocialMedia
+	profileImg="https://cdn.discordapp.com/avatars/96396806367227904/eca3bc8a3eeea3873c7e4be4f0b00210.png"
+  role="Logo Design"
+	url="https://discordapp.com/"
+	username="goilup#3147" />
+</div>
+} />
+//when I copied them all the user profile links ended in ?size=128
+
+const github =
+<Popup title="GitHub" children={
+<div>
+<SocialMedia
+	profileImg="https://avatars1.githubusercontent.com/u/54699262?s=200&v=4"
+  role="Repo"
+	url="https://github.com/OregonSmashUltimate/OregonSmashUltimate.github.io"
+	username="OregonSmashUltimate.github.io"/>
+<hr />
+<SocialMedia
+	profileImg="https://avatars0.githubusercontent.com/u/10984092?s=460&v=4"
+  role="Project Manager"
+	url="https://github.com/malin383"
+	username="malin383"/>
+<hr />
+<SocialMedia
+	profileImg="https://avatars1.githubusercontent.com/u/1504375?s=460&v=4"
+  role="Developer Lead"
+	url="https://github.com/cyril-vandyke"
+	username="cyril-vandyke"/>
+<hr />
+<SocialMedia
+	profileImg="https://avatars3.githubusercontent.com/u/21343049?s=460&v=4"
+  role="Lead Developer"
+	url="https://github.com/jCallon"
+	username="jCallon"/>
+<hr />
+<SocialMedia
+	profileImg="https://avatars3.githubusercontent.com/u/253703?s=460&v=4"
+  role="Human Database"
+	url="https://github.com/giantrobotbee"
+	username="giantrobotbee"/>
+<hr />
+<SocialMedia
+	profileImg="https://avatars2.githubusercontent.com/u/29513096?s=460&v=4"
+  role="Logo Design"
+	url="https://github.com/bananyog"
+	username="bananyog"/>
+</div>
+}/>
