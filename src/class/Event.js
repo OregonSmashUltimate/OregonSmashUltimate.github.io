f50395c36036e29a074daf16efc3e26de688c72d
jCallon <jascallon99@gmail.com>
2019-10-03 17:28:26 -0700

    Links reimplemented with icons.

--- src/class/Event.js
+++ src/class/Event.js
@@ -6,37 +6,41 @@ function Event(props){
       <h2>{props.name}</h2>
       <hr />
       <p>
+        <a href={props.streamURL !== "" && props.streamURL}
+          class={props.streamURL === "" && "dead" || props.streamURL !== "" && "icon"}
+          id="twitch"
+          target="_blank"
+          rel="noopener noreferrer"
+          alt="Icon kindly provided by fontawesome.com"></a>
+
+        <a href={props.websiteURL !== "" && props.websiteURL} 
+          class={props.websiteURL === "" && "dead" || props.websiteURL !== "" && "icon"}
+          id="generic"
+          target="_blank"
+          rel="noopener noreferrer"
+          alt="Icon kindly provided by fontawesome.com"></a>
+
+        <a href={props.facebookURL !== "" && props.facebookURL}
+          class={props.facebookURL === "" && "dead" || props.facebookURL !== "" && "icon"}
+          id="facebook"
+          target="_blank"
+          rel="noopener noreferrer"></a>
+
+        <a href={props.twitterURL !== "" && props.twitterURL}
+          class={props.twitterURL === "" && "dead" || props.twitterURL !== "" && "icon"}
+          id="twitter"
+          target="_blank"
+          rel="noopener noreferrer"></a>
+        <br />
+
         {props.time}<br />
         <a href={props.mapURL}
            target="_blank"
            rel="noopener noreferrer">{props.location}</a><br />
 
-        <a href={props.streamURL != "" && props.streamURL}
-           class={props.streamURL === "" && "strikethrough"}
-           target="_blank"
-           rel="noopener noreferrer">Stream</a>
-        &nbsp;|&nbsp; 
-        <a href={props.websiteURL != "" && props.websiteURL} 
-           class={props.websiteURL === "" && "strikethrough"}
-           target="_blank"
-           rel="noopener noreferrer">Website</a>
-        &nbsp;|&nbsp; 
-        <a href={props.facebookURL != "" && props.facebookURL}
-           class={props.facebookURL === "" && "strikethrough"}
-           target="_blank"
-           rel="noopener noreferrer">facebook</a>
-        &nbsp;|&nbsp; 
-        <a href={props.twitterURL != "" && props.twitterURL}
-           class={props.twitterURL === "" && "strikethrough"}
-           target="_blank"
-           rel="noopener noreferrer">Twitter</a>
-        <br />
+        {props.venueFee === "" && "Venue: Free" || props.venueFee !== "" && "Venue: $" + props.venueFee}
 
-        {props.venueFee === "0" && "Venue: Free"}
-        {props.venueFee != "0" && "Venue: $" + props.venueFee}
-
-        {props.entryFee === "0" && " | Entry: Free"}
-        {props.entryFee != "0" && " | Entry: $" + props.entryFee}
+        {props.entryFee === "" && " | Entry: Free" || props.entryFee !== "" && " | Entry: $" + props.entryFee}
         <br />
         Schedule: {props.schedule}<br />
         Notes: {props.notes}
