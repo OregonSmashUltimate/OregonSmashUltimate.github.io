8f9bac9bf469ba44236a1a3af006425ed99d32e9
jCallon <jcallon@LAPTOP-H9M83EFO.localdomain>
2020-01-03 19:06:05 -0800

    Can now toggle text size that will be remembered cross-session.

--- src/class/Collapsible.js
+++ src/class/Collapsible.js
@@ -4,20 +4,21 @@ class Collapsible extends React.Component{
 	constructor(props){
 		super(props);
 		this.state = {open: 1};
+    this.toggle = this.toggle.bind(this);
 	}
-  togglePanel(){this.setState({open: !this.state.open})}
+  toggle(){this.setState({open: !this.state.open});}
 	render(){
-		return (	
+		return(	
       <div>
-        <div onClick={this.togglePanel} style='cursor: pointer'>
-          <li class={this.state.open === 0 && "icon plus" ||
-                     this.state.open === 1 && "icon minus"}
-              alt="Icon kindly provided by fontawesome.com"></li>
+        <div onClick={this.toggle} style="cursor: pointer">
+          <li class={!this.state.open && "icon plus" ||
+                      this.state.open && "icon minus"}
+              alt="Icon kindly provided by fontawesome.com"/>
           <h2>{this.props.title}</h2>
         </div>
 
-        <div style={this.state.open === 0 && "display: none" ||
-                    this.state.open === 1 && "display: block"}>
+        <div style={!this.state.open && "display: none" ||
+                     this.state.open && "display: block"}>
           {this.props.children}
         </div>
       </div>
