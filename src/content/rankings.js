8439db0b302d4d6b996076d7e8459fdaf5379ea2
Niraj Mali <nirajlmali@gmail.com>
2020-03-02 16:43:24 -0800

    added rankings image to site

--- src/content/rankings.js
+++ src/content/rankings.js
@@ -3,7 +3,11 @@ import React from 'react';
 const rankings = 
 <div>
 <h3>Rankings are kept track of on <a href="https://smash.gg/rankings/super-smash-bros-ultimate/series/oregon" target="_blank" rel="noopener noreferrer"><u>smash.gg</u></a></h3>
+<img src="https://pbs.twimg.com/media/ENUeT09U8AAR5s1?format=jpg&name=4096x4096" alt="Oregon Winter 2019 PR" width="115%" height="115%"></img>
 <p>Power Rankings are created every 3 months by a panel made up of members in the community. You can find out more about rankings on our Discord and Facebook pages!</p>
+
 </div>
+
+
 
 export default rankings;
