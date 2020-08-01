c481b77b696aa3a5d6879fb2fdda13f0ed140725
jCallon <jcallon@LAPTOP-H9M83EFO.localdomain>
2020-08-01 14:24:30 -0700

    Made home colors dynamic

--- src/page/home.js
+++ src/page/home.js
@@ -15,19 +15,19 @@ export default function Home(){
           the online community!
         </h2>
       </Alert>
-      <Jumbotron className="text-center bg-dark text-light" id="jumbotron-home" data-aos="fade-up">
+      <Jumbotron className="text-center" id="jumbotron-home" data-aos="fade-up">
         <h1>Looking to join the Oregon Smash Ultimate Community?</h1>
         <hr></hr>
         <p>
             You've come to the right place!<br />
-            Join us on our&nbsp; 
-            <u><b><a href="https://discord.gg/K8nv9pt" target="_blank" rel="noopener noreferrer">Discord</a></b></u>
-            &nbsp;and&nbsp;
-            <u><b><a href="https://www.facebook.com/groups/499672353506994/" target="_blank" rel="noopener noreferrer">Facebook</a></b></u>
-            !</p>
-          <a href="https://discord.gg/K8nv9pt"><img alt="Not found" src="https://discord.com/assets/f8389ca1a741a115313bede9ac02e2c0.svg" id="discord-link-img"/></a>
-          <a href="https://facebookbrand.com/wp-content/uploads/2019/10/flogo_RGB_HEX-BRC-Site-250.png?w=125&h=125"><img alt="Not found" src="https://img.icons8.com/fluent/48/000000/facebook-new.png" id="facebook-link-img"/></a>
-          
+            Join us on our
+            <br/>
+            <div style={{textAlign: "center"}}>
+              <a href="https://discord.gg/K8nv9pt" target="_blank" rel="noopener noreferrer"><li class="icon discord"/> Discord</a>
+              &nbsp;|&nbsp;
+              <a href="https://www.facebook.com/groups/499672353506994/" target="_blank" rel="noopener noreferrer"><li class="icon facebook"/> Facebook</a>
+            </div>
+            </p>
       </Jumbotron>
     </div>
   );
