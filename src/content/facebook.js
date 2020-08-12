050ae6b8a781ae7b595d81627ecaff9bbb24853e
jCallon <jcallon@LAPTOP-H9M83EFO.localdomain>
2020-08-11 21:21:08 -0700

    Simulating all commits of jazz-pin-events

--- src/content/facebook.js
+++ src/content/facebook.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import Popup from '../class/Popup.js';
+import React, {useState} from 'react';
+import Modal from 'react-bootstrap/Modal';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 import SocialMedia from '../class/SocialMedia.js';
 
-const facebook = 
-<Popup title="facebook" children={
-<SocialMedia
-    profileImg="https://en.facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png"
-    role="Community"
-    url="https://www.facebook.com/groups/499672353506994/"
-    username="Oregon Ultimate Facebook Group"/>}/>
+function Facebook(){ 
+  const [show, setShow] = useState(false);
 
-export default facebook;
+  const handleShow  = () => setShow(true);
+  const handleClose = () => setShow(false);
+
+  return(
+  <>
+    <NavDropdown.Item onClick={handleShow}>
+      <li class="icon facebook" /> Facebook
+    </NavDropdown.Item>
+
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Facebook</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <SocialMedia
+          profileImg="https://en.facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png"
+          role="Community"
+          url="https://www.facebook.com/groups/499672353506994/"
+          username="Oregon Ultimate Facebook Group"/>
+      </Modal.Body>
+    </Modal>
+  </>
+  );
+}
+
+export default Facebook;
---
+++ src/content/weekly.js
@@ -0,0 +1,84 @@
+import React from 'react';
+
+import Event from '../class/Event.js';
+
+//see ../class/Event.js for expected formatting
+
+var weekly = [
+<Event
+  uid="1"
+  name="Dropzone"
+  time="Sundays from 12:00pm-6:00pm"
+  mapURL="https://goo.gl/maps/ir4vbcYbpTB8aWPk8"
+  location="CCG House, Vancouver, WA"
+  streamURL="https://twitch.tv/teamsolidarity"
+  websiteURL="https://www.teamsolidarity.info/dropzone"
+  facebookURL=""
+  twitterURL="https://twitter.com/dropzone_ts"
+  venueFee="5"
+  entryFee="5 per bracket"
+  schedule="Doors Open @ 12:00pm | Doubles @ 12:30pm | Singles @ 2:00pm"
+  notes="Spectate and play friendlies for free!"/>,
+
+<Event
+  uid="2"
+  name="Topdeck Tuesdays"
+  time="Tuesdays from 5:30pm-9:00pm"
+  mapURL="https://goo.gl/maps/A6SzTLTsPdc1rrzu8"
+  location="The Yugioh Outlet, Gresham, OR"
+  streamURL=""
+  websiteURL=""
+  facebookURL=""
+  twitterURL="https://twitter.com/ORchapter6"
+  venueFee="5"
+  entryFee="5"
+  schedule="Singles @ 6:00pm"
+  notes="Spectate and play friendlies for free!"/>,
+
+<Event
+  uid="3"
+  name="Cascade"
+  time="Wednesdays starting 6:00pm"
+  mapURL="https://g.page/Gongaii?share"
+  location="Gongaii Games, Inc., Beaverton, OR"
+  streamURL="https://twitch.tv/pdxcascade"
+  websiteURL=""
+  facebookURL=""
+  twitterURL="https://twitter.com/PDXCascade"
+  venueFee="5"
+  entryFee="5"
+  schedule="Singles @ 6:00pm"
+  notes="Side Brackets @ 5:00 PM, vote for the event on the linked Twitter; Spectate and play friendlies for free!"/>,
+
+<Event
+  uid="4"
+  name="Sacred Sanctuary"
+  time="Fridays from 5:30pm-9:00pm"
+  mapURL="https://goo.gl/maps/WjiKEBNZ2QRwLEbj7"
+  location="Room SS 121, PCC Sylvania Campus, Portland, OR"
+  streamURL="https://twitch.tv/UnitySmash"
+  websiteURL=""
+  facebookURL=""
+  twitterURL="https://twitter.com/TeziiMewtwo"
+  venueFee="5"
+  entryFee="5 per bracket"
+  schedule="Singles @ 6:00pm"
+  notes="Spectate and play friendlies for free!"/>,
+
+<Event
+  uid="5"
+  name="Salem Smashfest"
+  time="Saturdays starting 11:30pm"
+  mapURL="https://goo.gl/maps/N6gJ79SQAyKNqBLEA"
+  location="Corban University, Salem, OR 97317"
+  streamURL="https://twitch.tv/salemsmashfests"
+  websiteURL=""
+  facebookURL="https://www.facebook.com/groups/SalemSmashfests/"
+  twitterURL="https://twitter.com/SalemSmashfests"
+  venueFee="5"
+  entryFee="5 for Singles, $2 for Doubles"
+  schedule="Singles @ 1:30pm, Doubles @ 4:30pm"
+  notes="Free brackets for fighting games, such as BlazBlue Cross-Tag Battle, Tekken 7, and Guilty Gear Xrd -Rev2-"/>
+];
+
+export default weekly;
