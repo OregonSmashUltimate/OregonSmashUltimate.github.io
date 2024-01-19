41614aefbbadffc2567fffc8292d2e60a1dcb427
Arthur <meleearthur@gmail.com>
2024-01-18 18:39:56 -0800

    attempt number 2 to fix this

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Page from './class/Page';
+import NavBar from './class/NavBar.js'
+import Home from './page/home.js'
+import GettingStarted from './page/getting-started.js';
+import Events from './page/events.js';
+import Rankings from './page/rankings.js';
+import{
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
 
 //Animate On Scroll
 import "aos/dist/aos.css";
@@ -10,6 +20,15 @@ function App() {
   return (
     <div className="App">
       <Page/>
+      <Router>
+				<NavBar />
+				<Switch>
+					<Route exact path="/"><Home /></Route>
+					<Route exact path="/getting-started"><GettingStarted /></Route>
+					<Route exact path="/events"><Events /></Route>
+					<Route exact path="/rankings"><Rankings /></Route>
+				</Switch>
+			</Router>
     </div>
   );
 }
